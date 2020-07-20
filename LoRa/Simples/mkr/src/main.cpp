dc5a68592b2bbd2f77245d71df95c0b136fb3177
fabioformiga <60191810+fabioformiga@users.noreply.github.com>
2020-07-20 15:41:39 +0100

    Simple transmission
    adição dos ficheiros referentes a comunicação simples LORA entre MKR e RPI3b+

---
+++ LoRa/Simples/mkr/src/main.cpp
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <LoRa.h>
+
+int counter = 0;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  Serial.println("LoRa Sender");
+
+  if (!LoRa.begin(868100000)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+   LoRa.setSpreadingFactor(7);
+}
+
+void loop() {
+  Serial.print("Sending packet: ");
+  Serial.println(counter);
+
+  // send packet
+  LoRa.beginPacket();
+  LoRa.print("hello ");
+  LoRa.print(counter);
+  LoRa.endPacket();
+
+  counter++;
+
+  delay(5000);
+}
